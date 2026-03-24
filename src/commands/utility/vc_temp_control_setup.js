effba739c8a90d1e66459754a61f2b09fb86372a
Omar Emam <emamomar857@gmail.com>
2026-03-24 11:49:11 +0200

    AKDGJ

--- src/commands/utility/vc_temp_control_setup.js
+++ src/commands/utility/vc_temp_control_setup.js
@@ -34,7 +34,8 @@ module.exports = {
         const banner = new AttachmentBuilder(path.join(__dirname, '../../assets/1234.png'), { name: '1234.png' });
 
         const embed = new EmbedBuilder()
-            .setColor(client?.config?.colors?.primary || THEME?.COLORS?.PRIMARY || '#111827');
+            .setColor(client?.config?.colors?.primary || THEME?.COLORS?.PRIMARY || '#111827')
+            .setDescription(' ');
 
         const row1 = new ActionRowBuilder().addComponents(
             new ButtonBuilder().setCustomId('tvcp_lock').setStyle(ButtonStyle.Secondary).setEmoji('▫️').setLabel('Lock'),
